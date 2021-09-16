1205a0072cf0a4d07c13dcb3702ecfcf110ae6d4
Jyothi959 <89759853+Jyothi959@users.noreply.github.com>
2021-09-16 14:24:37 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,112 +1,155 @@
-#include <calculator_operations.h>
-
-/* Status of the operation requested */
-#define VALID   (1)
-#define INVALID (0)
-
-/* Calculator operation requested by user*/
-unsigned int calculator_operation = 0;
-
-/* Operands on which calculation is performed */
-int calculator_operand1 = 0;
-int calculator_operand2 = 0;
-
-/* Valid operations */
-enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE, EXIT };
-
-/* Display the menu of operations supported */
-void calculator_menu(void);
-/* Verifies the requested operations validity */
-int valid_operation(int operation);
+/**
+ * @file main.c
+ * @author Jyothi J Shankar ()
+ * @brief project to control LED by two switches
+ * @version 0.1
+ * @date 2021-04-26
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 
-/* Start of the application */
-int main(int argc, char *argv[])
+ /**
+  * @brief contain macros and headers
+  * 
+  */
+#include<avr/io.h>
+#include<avr/interrupt.h>
+#include "Activity1.h"
+#include "Activity2.h"
+#include "Activity3.h"
+#include "Activity4.h"
+/**
+ * @brief Main function where the code execution starts
+ * 
+ * @return  Return 0 if the program completes successfully
+ */
+
+int main(void)
 {
-    printf("\n****Calculator****\n");
+    initialise_ports_a1();
+    initialise_ports_a2();
+    initialise_ports_a3();
+    InitADC();
+    USARTInit(103);
+    uint16_t temp;
+    int a=0;int b=0;int c=0;int d=0;int e=0;
     while(1)
     {
-        calculator_menu();
+        if(!(PIND &(1<<PIND2)))
+        {
+            LED_PORTC2_HIGH();
+            if(!(PIND &(1<<PIND3)))
+
+            {
+                    temp=ReadADC(0);
+                    if(temp<895)
+                    {
+                        LED_PORTC3_HIGH();
+                        if(temp>0  && temp<250)
+                            {
+                                OCR1A=980;
+                                if(a==0)
+                                {
+                                 unsigned char data_value[]="TEMPERATURE IS BETWEEN -10 AND 0 DEG \n";int i=0;
+                                while(data_value[i]!=0)
+                              {
+
+                               USARTWrite(data_value[i]);
+                                i++;
+                                a++;
+                                b=0;c=0;d=0;e=0;
+                            }
+                                }
+                            }
+                            else if(temp>=250 && temp<500)
+                            {
+                                OCR1A=700;
+                                if(b==0)
+                                {
+                                  int i=0;
+                                 unsigned char data_value[]="TEMPERATURE IS BETWEEN 0 AND 10 DEG \n";   
+                                 while(data_value[i]!=0)
+                              {
+
+                               USARTWrite(data_value[i]);
+                                i++;
+                                b++;a=0;c=0;d=0;e=0;
+                            }
+                            }
+                            }
+                            else if(temp>=500 && temp<750)
+                            {
+                                OCR1A=400;
+                               if(c==0)
+                               {
+                                unsigned char data_value[]="TEMPERATURE IS BETWEEN 10 AND 20 DEG \n"; 
+                                int i=0;   
+                                while(data_value[i]!=0)
+                              {
+
+                               USARTWrite(data_value[i]);
+                                i++;
+                                c++;a=0;b=0;d=0;e=0;
+                            }
+                            }
+                            }
+                            else if(temp>=750 && temp<894)
+                            {
+                                OCR1A=200;
+                                if(d==0)
+                                {
+                                 int i=0;
+                                 unsigned char data_value[]="TEMPERATURE IS BETWEEN 20 AND 25 DEG \n";
+                                 while(data_value[i]!=0)
+                              {
+
+                               USARTWrite(data_value[i]);
+                                i++;
+                                d++;a=0;b=0;c=0;e=0;
+                            }
+                            }
+                    }
+                    }
+                    else
+                        {
+                        LED_PORTC3_LOW();
+                         OCR1A=0;
+                                if(e==0)
+                                {
+                                int i=0;
+                                 unsigned char data_value[]="TEMPERATURE IS ABOVE 25 DEG \n";
+                                 while(data_value[i]!=0)
+                              {
+
+                               USARTWrite(data_value[i]);
+                                i++;
+                                e++;a=0;b=0;c=0;d=0;
+                            }
+                            }
+
+                        }
+            }
+
+
+                else
+                    {
+                    OCR1A=0;
+                    LED_PORTC3_LOW();
+                    }
+        }
+
+        else
+        {
+            LED_PORTC2_LOW();
+            LED_PORTC3_LOW();
+            OCR1A=0;
+        }
     }
+
+
+ return 0;
 }
 
-void calculator_menu(void)
-{
-    printf("\nAvailable Operations\n");
-    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Exit");
-    printf("\n\tEnter your choice\n");
-   
-     __fpurge(stdin);
-    scanf("%d", &calculator_operation);
 
-    if(EXIT == calculator_operation)
-    {
-        printf("\nThank you. Exiting the Application\n");
-        exit(0);
-    }
-
-    if(INVALID != valid_operation(calculator_operation))
-    {
-        printf("\n\tEnter your Numbers with space between them\n");
-        __fpurge(stdin);
-        scanf("%d %d", &calculator_operand1, &calculator_operand2);
-    }
-    else
-    {
-        printf("\n\t---Wrong choice---\nEnter to continue\n");
-        __fpurge(stdin);
-        getchar();
-        return;
-        
-    }
-    switch(calculator_operation)
-    {
-        case ADD:
-            printf("\n\t%d + %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            add(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case SUBTRACT:
-            printf("\n\t%d - %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            subtract(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case MULTIPLY:
-            printf("\n\t%d * %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            multiply(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case DIVIDE:
-            printf("\n\t%d / %d = %d\nEnter to continue", 
-            calculator_operand1, 
-            calculator_operand2,
-            divide(calculator_operand1, calculator_operand2));
-            
-            __fpurge(stdin);
-            getchar();
-            break;
-        case 5:
-            exit(0);
-            break;
-        default:
-            printf("\n\t---It should never come here---\n");
-    }
-}
-
-int valid_operation(int operation)
-{
-    /* Check if the operation is a valid operation */
-    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
-}
